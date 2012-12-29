0f8fefca80d7ffeba38be8e3734acd85542e455a
VariantF <sunjiayu_2006@126.com>
2012-12-29 23:08:34 +0800

    Initial Commit

---
+++ MD5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MD5
+//
+//  Created by 孙 嘉裕 on 12-12-29.
+//  Copyright (c) 2012年 孙 嘉裕. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
